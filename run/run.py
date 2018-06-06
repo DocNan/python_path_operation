8a530d532a17aa621ceebfd5ee956ae1046421fc
Nan <chunan@mail.ustc.edu.cn>
2018-06-06 09:09:16 +0000

    Update run.py

--- run/run.py
+++ run/run.py
@@ -11,15 +11,9 @@ path.append(os.path.abspath('../'))
 # f21()
 
 # directly import submodules
-# import mod_package.submod1.mod1 as sb1
-# sb1.f1()
+import mod_package.submod1.mod1 as sb1
+sb1.f1()
 
-# call f31 in mod3 from run
-# import mod_package.submod2.mod21 as mod21
-# mod21.f21()
-
-# import mod_package.submod4.mod21 as mod4
-# mod4.f21()
-
+# call f31 in mod3 from run, this will test addpath within modules
 import mod_package.submod3.mod3 as mod3
 mod3.f31()
